540cca5f6e4107ca9c4bbea8d767f7060bf4e08d
3018Ravindra <veerravindra190@gmail.com>
2026-02-17 23:07:53 +0530

    updating my first.py with python

--- first.py
+++ first.py
@@ -4,3 +4,17 @@ print("This is my first python program")
 print("i am learning python programming language")
 
 
+# my second program in python (Pthong Indentation)
+if 5 > 2:
+    print("five is greater than two")
+
+ #my third program is python variable
+x = 5
+y = "hello world"
+print(x)
+print(y)
+
+#my fourth pragram in python is python comments
+#this is a comment 
+print("this is a comment in python")
+
