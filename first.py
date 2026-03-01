229c7861184e7f994bf94a8fedb27b860ee912ef
3018Ravindra <veerravindra190@gmail.com>
2026-03-01 22:00:16 +0530

    formatting with f-string

--- first.py
+++ first.py
@@ -124,3 +124,8 @@ print(c)
 name = "ravi"
 age = 25
 print("my name is {} and i am {} years old".format(name, age))
+
+# my eighteenth program in python is python string formatting with f-string
+name = "ravi"
+age = 25
+print(f"my name is {name} and i am {age} years old")
