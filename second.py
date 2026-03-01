f40fb9294208781eed6e7d61c5bd0c3c640d5c0f
3018Ravindra <veerravindra190@gmail.com>
2026-03-01 21:49:48 +0530

    second problem is to print message with a variable

--- second.py
+++ second.py
@@ -4,3 +4,7 @@
 # my first problem in python is to print a message
 print("i am here to solve problems")
 print("this is for my cloud and devops journey")
+
+# my second problem in python is to print a message with a variable
+message = "i a solving problems with python"
+print(message)
