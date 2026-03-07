30e4cf2a3f3a161f8977ca945034363ae131e840
3018Ravindra <veerravindra190@gmail.com>
2026-03-07 15:42:10 +0530

    simple python

---
+++ DSA.py
@@ -0,0 +1,15 @@
+# my file for solving DSA Problems with python programming language
+
+# problem is to solve with a print statement
+print("Hello DSA")
+
+# Problem is to solve with a variable and a print statement
+name = "Ravindra"
+print(name)
+
+# Problem is to solve with a variable and a loop
+name = "Ravindra"
+for char in name:
+    print(char)
+
+    
